75ad576ba783f28b80850a343fd63677a61434d1
Ramil spiN <ramil_spin@mail.ru>
2021-12-15 16:56:21 +0300

    Added modules to client

---
+++ client/src/app/module/Brickset.ts
@@ -0,0 +1,12 @@
+import {Comment} from './Comment'
+export interface Brickset {
+  id?: number;
+  title: string;
+  caption: string;
+  inventory: string;
+  image?: File;
+  likes?: number;
+  likedUsers?: string[];
+  comments?: Comment[];
+  username?: string;
+}
